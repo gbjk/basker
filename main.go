aa646858d78566e2e6ec3a60e81aff6c3a04806d
Gareth Kirwan <gbjk@thermeon.com>
2016-04-26 16:22:42 +0100

    Added influxdb logging
    Still full of hardcoded assumptions, and pretty much dangerware

--- main.go
+++ main.go
@@ -8,32 +8,83 @@ import (
 	"strings"
 	"time"
 
+	idb "github.com/influxdata/influxdb/client/v2"
 	"github.com/valyala/fasthttp"
 )
 
 const (
-	target = "http://127.0.0.1:8100/"
+	// TODO - move to flag
+	influxdbUrl  = "http://192.168.99.101:8086"
+	influxdbName = "proxy_overhead"
 )
 
 var workers int
+var target string
+var label string
 
 func main() {
 
+	flag.StringVar(&label, "label", "", "influxdb label")
+	flag.StringVar(&target, "target", "", "http target uri incuding port")
 	flag.IntVar(&workers, "workers", 10, "Number of concurrent workers")
 	flag.Parse()
 
 	fmt.Println("Workers:", workers)
+	fmt.Println("Target:", target)
+	fmt.Println("Label:", label)
 
 	feedbackPipe := make(chan float64)
 
+	if target == "" {
+		log.Fatal("Please supply --target in the format http://127.1:8100/")
+	}
+
+	if label == "" {
+		log.Fatal("Please supply --label")
+	}
+
 	for i := 0; i < workers; i++ {
 		go hitTarget(feedbackPipe)
+	}
+
+	client, err := idb.NewHTTPClient(idb.HTTPConfig{
+		Addr: influxdbUrl,
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	q := idb.NewQuery(fmt.Sprintf("CREATE DATABASE %s", influxdbName), "", "")
+	// Ignoring errors, probably that the db already exists. TODO.
+	client.Query(q)
+
+	bp, err := idb.NewBatchPoints(idb.BatchPointsConfig{
+		Precision: "ns",
+		Database:  influxdbName,
+	})
+
+	if err != nil {
+		panic(err)
 	}
 
 	for {
 		select {
 		case timing := <-feedbackPipe:
 			fmt.Println("Got back: ", timing)
+			tags := map[string]string{
+				"backend": label,
+			}
+			fields := map[string]interface{}{
+				"overhead": timing,
+			}
+			pt, err := idb.NewPoint("proxy_testing", tags, fields, time.Now())
+			if err != nil {
+				panic(err)
+			}
+			bp.AddPoint(pt)
+			if err := client.Write(bp); err != nil {
+				panic(err)
+			}
 		}
 	}
 }
